8bbf6dd64e2fb26c71e5ed9ff34ecaf94c1bfeaa
zhujiruo <zhujiruo@foxmail.com>
2022-12-16 09:48:30 +0800

    ✨ feat(fetch Data): fetch with proxy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(()=>{
+    fetch('/api/fetchData')
+  },[])
   return (
     <div className={styles.container}>
       <Head>
